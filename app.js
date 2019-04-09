45ded21b63cb5f20088810b94ae95e313a711aaa
jayok <jerome.okeeffe@ensiie.fr>
2019-04-09 16:48:16 +0200

    feat(router) refactor controller -> router

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 let app = express();
+const jetpackRouter = require('./routers/jetpack');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app
-  .route('/jetpacks/:id?')
-  .get(require('./controller/Jetpack/GetJetpackController'))
-  .post(require('./controller/Jetpack/CreateJetpackController'));
+app.use('/jetpack', jetpackRouter);
+
+// app
+//   .route('/jetpacks/:id?')
+//   .get(require('./controller/Jetpack/GetJetpackController'))
+//   .post(require('./controller/Jetpack/CreateJetpackController'));
 
 app.listen(3000, function() {
   console.log('Example app listening on port 3000!');
